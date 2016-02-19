dbeb92490707cd3957458aca014c3385295d258a
Dan Bearl <dan.bearl@gmail.com>
2016-02-19 13:24:57 -0500

    Adding posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,19 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    if post.update_attributes(post_params)
+      redirect_to post, notice: "Post successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    post.destroy
+    redirect_to posts_path, notice: "Post successfully deleted."
+  end
+
   private
 
   def post_params
